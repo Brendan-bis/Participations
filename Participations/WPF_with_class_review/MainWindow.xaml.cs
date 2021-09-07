74e8ea317b6776cf9edb5bbbb737a8b184013067
Brendan Palmisano <Brendan.P.Palmisano-1@ou.edu>
2021-09-07 15:57:51 -0500

    Completed Fizz Buzz Example

---
+++ Participations/WPF_with_class_review/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_with_class_review
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtWordOrNumber.Clear();
+          
+        }
+
+        private void btnWordOrNumber_Click(object sender, RoutedEventArgs e)
+        {
+            string input = txtWordOrNumber.Text;
+            double value;
+            bool isNumber = double.TryParse(input, out value);
+            if (isNumber)
+            {
+                listBoxNumbers.Items.Add(value.ToString("N3"));
+            }
+            else
+            {
+                listBoxWords.Items.Add(input);
+            }
+            txtWordOrNumber.Clear();
+
+            
+        }
+    }
+}
