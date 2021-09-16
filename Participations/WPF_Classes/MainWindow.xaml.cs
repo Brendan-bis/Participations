6d6d28a5c82a2d39c372603d08354f9f5982940b
Brendan Palmisano <Brendan.P.Palmisano-1@ou.edu>
2021-09-16 16:05:28 -0500

    Completed participation over classes

--- Participations/WPF_Classes/MainWindow.xaml.cs
+++ Participations/WPF_Classes/MainWindow.xaml.cs
@@ -27,7 +27,67 @@ namespace WPF_Classes
 
         private void btnInput_Click(object sender, RoutedEventArgs e)
         {
+            bool isEverythingGood = true;
 
+
+            if (string.IsNullOrWhiteSpace(txtManufacturer.Text) == true)
+            {
+                isEverythingGood = false;
+                MessageBox.Show("You must enter a valid Manufacturer!");
+            }
+ 
+            if (string.IsNullOrWhiteSpace(txtName.Text) == true)
+            {
+                isEverythingGood = false;
+                MessageBox.Show("You must enter a valid Name!");
+            }
+            if (string.IsNullOrWhiteSpace(txtImage.Text) == true)
+            {
+                isEverythingGood = false;
+                MessageBox.Show("You must enter a valid Image!");
+            }
+
+            //double
+            string number = txtPrice.Text;
+            double value;
+            bool isNumber = double.TryParse(number, out value);
+            if (isNumber == false)
+            {
+                isNumber = false;
+                MessageBox.Show("You must enter a valid Price!");
+            }
+
+            if (isEverythingGood == false)
+            {
+                return;
+            }
+
+            Toy toys = new Toy()
+            {
+                Manufacturer = txtManufacturer.Text,
+                Name = txtName.Text,
+                Image = txtImage.Text,
+                Price = Convert.ToDouble(number),
+
+            };
+
+            lstToys.Items.Add(toys);
+            txtName.Clear();
+            txtManufacturer.Clear();
+            txtImage.Clear();
+            txtPrice.Clear();
+
+        }
+
+        private void lstToys_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Toy selectedToy = (Toy)lstToys.SelectedItem;
+            MessageBox.Show($"{selectedToy.ToString()} is found on aisle: {selectedToy.GetAisle().ToUpper()}");
+
+            
+            var uri = new Uri(selectedToy.Image);
+            var img = new BitmapImage(uri);
+            imgToy.Source = img;
         }
     }
 }
---
+++ Participations/WPF_Classes/Toy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes
+{
+    public class Toy
+    {
+        public string Manufacturer {get;set;}
+        public string Name {get;set;}
+        public double Price {get;set;}
+        public string Image {get;set;}
+        private string Aisle;
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Image = string.Empty;
+        }
+
+        public string GetAisle()
+        {
+            //Returns the first letter of the Manufacturer, capitalized with the value of the Price afterwards (with no punctuation like . or , or $).
+            string firstLetter = Manufacturer[0].ToString() + Price.ToString().Replace(".","");
+            
+            return firstLetter;
+        }
+        public override string ToString()
+        {
+            return $"{Manufacturer} - {Name}";
+        }
+    }
+}
