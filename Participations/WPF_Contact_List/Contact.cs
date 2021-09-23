2bc28cbf56cd20b2a5b4fc310895649f385a55b5
Brendan Palmisano <Brendan.P.Palmisano-1@ou.edu>
2021-09-23 15:09:12 -0500

    Completed Class

---
+++ Participations/WPF_Contact_List/Contact.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Contact_List
+{
+    public class Contact
+    {
+        // 1      2       3        4    5
+        //Id|FirstName|LastName|Email|Photo
+
+        public int ID{get;set;}
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+        public string Email{get;set;}
+        public string Photo{ get; set; }
+
+
+        public Contact(string line)
+        {
+            var pieces = line.Split('|');
+
+            ID = Convert.ToInt32(pieces[1]);
+            FirstName = pieces[2];
+            LastName = pieces[3];
+            Email = pieces[4];
+            Photo = pieces[5];
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+
+    }
+    
+}
