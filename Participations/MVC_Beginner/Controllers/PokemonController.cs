071087d7875cf6acd9d0c0e64c975ff226f0dd98
Brendan Palmisano <Brendan.P.Palmisano-1@ou.edu>
2021-11-23 10:18:23 -0600

    Completed Controller for Chuck

---
+++ Participations/MVC_Beginner/Controllers/PokemonController.cs
@@ -0,0 +1,44 @@
+﻿using MVC_Beginner.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Beginner.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            PokemonApi allpokemon;
+
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1100").Result;
+                allpokemon = JsonConvert.DeserializeObject<PokemonApi>(json);
+            }
+
+
+            return View(allpokemon.results);
+        }
+        public ActionResult Info(string id)
+        {
+            Pokemoninfo info;
+
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{id}").Result;
+                info = JsonConvert.DeserializeObject<Pokemoninfo>(json);
+            }
+
+
+            return View(info);
+
+
+        }
+    }
+}
---
+++ Participations/MVC_Beginner/Models/Pokemoninfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Beginner.Models
+{
+    public class Pokemoninfo
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public int height { get; set; }
+        public int order { get; set; }
+        public int weight { get; set; }
+
+
+
+    }
+}
